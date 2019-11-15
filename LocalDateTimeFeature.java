d2feb587cd349b5db2852740314e9e614329f528
Krishnamoorthy <krishna192168@gmail.com>
2019-11-15 10:59:17 +0530

    LoacalDateTime

--- LocalDateTimeFeature.java
+++ LocalDateTimeFeature.java
@@ -1,10 +1,18 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 public class Simple{
+public static DateTimeFormatter formatter(String format)
+{
+DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+return formatter;
+}
 public static void main(String args[]){
 LocalDateTime dateTime = LocalDateTime.now();
 System.out.println("DateTime=>"+dateTime);
-DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd:MM:yyyy HH:mm:ss");
-String currentDateTime = dateTime.format(dateFormat);
+DateTimeFormatter dateWithTime = Simple.formatter("dd:MM:yyyy HH:mm:ss");
+String currentDateTime = dateTime.format(dateWithTime);
 System.out.println("DD:MM:YYYY HH:MM:SS=>"+currentDateTime);
+DateTimeFormatter dateOnly = Simple.formatter("dd:MM:yyyy");
+String currentDate = dateTime.format(dateOnly);
+System.out.println("DD:MM:YYYY=>"+currentDate);
 }}
